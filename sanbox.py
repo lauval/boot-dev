65b5efc386da9d89aa4c8937c15802d0d63c884c
Laurent <LaurentValentin@outlook.com>
2026-02-13 08:27:30 +0400

    completed inheritance

--- sanbox.py
+++ sanbox.py
@@ -1,38 +1,7 @@
-import random
+stuff = [1,2,3,4,5]
 
-SUITS = ["Hearts", "Diamonds", "Clubs", "Spades"]
-RANKS = [
-    "Ace",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "Jack",
-    "Queen",
-    "King",
-]
 
-# cards = [(suit, rank) for suit in SUITS for rank in RANKS]
-
-cards = []
-
-# test
-for suit in SUITS:
-    for rank in RANKS:
-        cards.append((suit, rank))
-
-print(len(cards))
-print(cards)
-
-print("\n Now shuffling")
-random.shuffle(cards)
-print(cards)
-
-print("Dealing from the top of the deck")
-top_of_deck = cards.pop()
-print(top_of_deck)
+for element in stuff:
+    to_keep = stuff.copy()
+    to_keep.remove(element)
+    print(to_keep)
