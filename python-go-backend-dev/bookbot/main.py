0957f79351e3ec201daab16cf0e0a12f8e99aeac
Laurent <LaurentValentin@outlook.com>
2026-02-19 20:53:50 +0400

    final edit to pass boot-dev test. Was a fun one

--- python-go-backend-dev/bookbot/main.py
+++ python-go-backend-dev/bookbot/main.py
@@ -6,19 +6,18 @@ from stats import (
     helper_sort,
 )
 
+import sys
 
-def main():
+def main(path):
 
-    FILEPATH = "books/frankenstein.txt"
-
-    book_contents = get_book_text(FILEPATH)
+    book_contents = get_book_text(path)
     word_count = count_words(book_contents)
     character_count = count_unique_characters(book_contents)
     dicts = convert_to_list_of_dicts(character_count)
     dicts.sort(reverse=True, key=helper_sort)
 
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {path}...")
     print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
     print("--------- Character Count -------")
@@ -30,4 +29,15 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    # set placeholder path to resolve unbound error
+    path = None
+
+    # check if the user specified a path as an argument
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1) # exit code 1: failure, 0: success
+    else:
+        # user-defined path is then passed to main
+        path = sys.argv[1]
+
+    main(path)
