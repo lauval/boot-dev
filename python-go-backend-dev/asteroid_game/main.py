d12ef78b9255200ad4f4b9447ebd79a6a6964ce1
Laurent <LaurentValentin@outlook.com>
2026-02-22 17:30:14 +0400

    added player rendering and rotation on keyboard inputs

--- python-go-backend-dev/asteroid_game/main.py
+++ python-go-backend-dev/asteroid_game/main.py
@@ -1,6 +1,7 @@
 import pygame  # game engine
 from logger import log_state  # for automated boot dev testing
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH  # dimensions
+from player import Player
 
 VERSION = pygame.ver
 
@@ -15,13 +16,16 @@ def main():
 
     clock = pygame.time.Clock()  # to set a refresh rate (see end of game loop below)
 
-    dt = 0
+    dt = 0  # time between each screen refresh
 
     # define screen params
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     # set condition for game loop
     run_game = True
+
+    # initialise player
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     # run the game loop
     while run_game:
@@ -33,11 +37,20 @@ def main():
 
         screen.fill("black")
 
-        pygame.display.flip()  # refresh the screen
+        # update player
+        player.update(dt)
 
-        dt = clock.tick(60) / 1000 # clock.tick() returns the time in milliseconds since it was last called
+        # draw player to the screen
+        player.draw(screen)
 
-        print(dt)
+        # refresh the screen
+        pygame.display.flip()
+
+        # force the clock to wait until 1/60th of a second has passed before starting the newest loop
+        dt = (
+            clock.tick(60) / 1000
+        )  # clock.tick() returns the time in milliseconds since it was last called
+
 
 if __name__ == "__main__":
     main()
